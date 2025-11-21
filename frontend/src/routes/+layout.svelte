7546aad943144fb6aa793c4476d31bb898474b82
sherstnew <sherstnev.denis.v@gmail.com>
2025-11-21 17:06:27 +0300

    svelte init

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { ModeWatcher } from "mode-watcher";
+	import ThemeSwitcher from '$lib/components/themeswitcher.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<!-- <link rel="icon" href={favicon} /> -->
+	<title>lutra</title>
 </svelte:head>
 
+<ModeWatcher />
+<header class="p-5 flex justify-end">
+	<ThemeSwitcher />
+</header>
 {@render children()}
